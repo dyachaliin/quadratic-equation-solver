a000cb59606ae3d22be1d1d7622f2cef545e86cc
dyachaliin <dyachaliin@gmail.com>
2022-02-17 15:14:30 +0200

    General algorithm for solving square equations

--- quadratic-equation-solver/main.swift
+++ quadratic-equation-solver/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func quadraticEquationSolver(a: Double, b: Double, c: Double) {
+    var discriminantRes: [Double] = []
+    print("Equation is: (\(a)) x^2 + (\(b)) x + (\(c)) = 0")
+    
+    let discriminant = b * b - 4 * a * c
+    print("Discriminant is \(discriminant)")
+    
+    if discriminant > 0 {
+        let x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
+        let x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
+        discriminantRes.append(x1)
+        discriminantRes.append(x2)
+    } else if discriminant == 0 {
+        let x = (-b) / (2 * a)
+        discriminantRes.append(x)
+    }
+    print("There are \(discriminantRes.count) roots")
+    if discriminantRes.count == 1 {
+        print("x1 = \(discriminantRes[0])")
+    } else if discriminantRes.count == 2 {
+        print("x1 = \(discriminantRes[0])")
+        print("x2 = \(discriminantRes[1])")
+    }
+    
+}
 
